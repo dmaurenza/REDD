768e1f0781333458e493229be218aae9ebf8b438
dmaurenza <dmaurenza@gmail.com>
2023-03-03 12:38:22 -0300

    Atualização 03/03

--- Script Inicial.R
+++ Script Inicial.R
@@ -1,35 +1,8 @@
-install.packages("rgdal", dependencies = T)
-library(rgdal)
 library(sf)
 library(raster)
 library(terra)
 library(tidyverse)
 library(geobr)
-# RJ <- untar(tarfile = "FBDS/RJ.tar")
-# BRoxo <- read_sf("FBDS/BELFORD_ROXO/USO/RJ_3300456_USO.shp")
-# CMacacu <- read_sf("FBDS/CACHOEIRAS_DE_MACACU/USO/RJ_3300803_USO.shp")
-# DCaxias <- read_sf("FBDS/DUQUE_DE_CAXIAS/USO/RJ_3301702_USO.shp")
-# Guapimirim <- read_sf("FBDS/GUAPIMIRIM/USO/RJ_3301850_USO.shp")
-# Itaborai <- read_sf("FBDS/ITABORAI/USO/RJ_3301900_USO.shp")
-# Itaguai <- read_sf("FBDS/ITAGUAI/USO/RJ_3302007_USO.shp")
-# Japeri <- read_sf("FBDS/JAPERI/USO/RJ_3302270_USO.shp")
-# Mage <- read_sf("FBDS/MAGE/USO/RJ_3302502_USO.shp")
-# Marica <- read_sf("FBDS/MARICA/USO/RJ_3302700_USO.shp")
-# Mesquita <- read_sf("FBDS/MESQUITA/USO/RJ_3302858_USO.shp")
-# Nilopolis <- read_sf("FBDS/NILOPOLIS/USO/RJ_3303203_USO.shp")
-# Niteroi <- read_sf("FBDS/NITEROI/USO/RJ_3303302_USO.shp")
-# NIguacu <- read_sf("FBDS/NOVA_IGUACU/USO/RJ_3303500_USO.shp")
-# Paracambi <- read_sf("FBDS/PARACAMBI/USO/RJ_3303609_USO.shp")
-# Petropolis <- read_sf("FBDS/PETROPOLIS/USO/RJ_3303906_USO.shp")
-# Queimados <- read_sf("FBDS/QUEIMADOS/USO/RJ_3304144_USO.shp")
-# RBonito <- read_sf("FBDS/RIO_BONITO/USO/RJ_3304300_USO.shp")
-# SGoncalo <- read_sf("FBDS/SAO_GONCALO/USO/RJ_3304904_USO.shp")
-# SMeriti <- read_sf("FBDS/SAO_JOAO_DE_MERITI/USO/RJ_3305109_USO.shp")
-# Seropedica <- read_sf("FBDS/SEROPEDICA/USO/RJ_3305554_USO.shp")
-# Tangua <- read_sf("FBDS/TANGUA/USO/RJ_3305752_USO.shp")
-# Rio <- read_sf("FBDS/RIO_DE_JANEIRO/USO/RJ_3304557_USO.shp")
-# metro <- rbind(BRoxo, CMacacu, DCaxias, Guapimirim, Itaborai, Itaguai, Japeri, Mage, Marica, Mesquita, Nilopolis, Niteroi, NIguacu, Paracambi, Petropolis, Queimados, RBonito, SGoncalo, SMeriti, Seropedica, Tangua, Rio)
-
 
 ## Leitura de dados e primeiros ajustes
 
@@ -47,21 +20,18 @@ metro <- rj %>%
 
 # Dados SIGEF
 # Fiz o Download no site https://certificacao.incra.gov.br/csv_shp/export_shp.py
-# "Imovel certificado sigef privado". RIo de Janeiro. Este dado refere-se às certificações feitas pela norma 3
+# "Imovel certificado sigef privado". Rio de Janeiro. Este dado refere-se às certificações feitas pela norma 3
 # O arquivo "Imovel certificado SNCI privado" refere-se às certificações feitas pelas normas 1 e 2. Devemos decidir sobre isso. Aqui consta a análise somente com a norma 3
 
 
 sigef_3 <- read_sf("SIGEF/Norma 3/Sigef Privado_RJ.shp")
 
+# transformando dados sigef em CRS wgs84
 
-# Aqui é uma dúvida que tenho. Temos que projetar o raster no mesmo CRS de SIGEF, porém CRS em SIRGAS não fornece a resolução em metros, e isso esta confundindo o calculo da área. Será que projetamos para Albers?
+sigef_3 <- st_transform(sigef_3, 4326)
 
-utm23s <- "+init=epsg:31983"
-sf::st_transform(sigef_3, CRS = utm23s)
-
-
+# Existe o dado de SIGEF norma 1 e 2
 #sigef_1_2 <- read_sf("SIGEF/Norma 1 2/Imóvel certificado SNCI Privado_RJ.shp")
-sf_use_s2(T)
 
 
 # Dados Mapbiomas
@@ -69,19 +39,10 @@ sf_use_s2(T)
 # https://storage.googleapis.com/mapbiomas-public/brasil/collection-7/lclu/coverage/brasil_coverage_2021.tif
 
 BR <- terra::rast("Mapbiomas/brasil_coverage_2021.tif")
-albers <- "+proj=aea +lat_1=-5 +lat_2=-42 +lat_0=-32 +lon_0=-60 +x_0=0 +y_0=0 +ellps=aust_SA +units=m +no_defs"
-raster::projectRaster(BR, crs = albers)
-raster::projection(BR)
-crs(BR) <- albers
-BR
-b <- rast(ncols=94, nrows=124, xmin=-944881, xmax=935118, ymin=4664377, ymax=7144377, crs=albers)
-terra::project(BR, y = b)
 
+# transformando dados mapbiomas em CRS wgs84 (igual dados sigef)
+crs(BR) <- crs(sigef_3)
 
-plot(BR)
-values(BR$brasil_coverage_2021)
-crs(BR) <- "+proj=longlat +datum=WGS84 +no_defs"
-raster:projection(BR)
 
 # Filtrando dados espaciais de SIGEF para a região metropolitana do Rio de Janeiro
 
@@ -108,43 +69,41 @@ for (i in metro_cod){
 
 # Selecionando os dados espaciais do Mapbiomas que ocorrem na região metropolitana do Rio de Janeiro
 
-
-raster::projectRaster(BR, crs = utm23s)
-
-values(BR)
-
 RJ_raster <- crop(BR, ext(sigef_3))
 
 plot(RJ_raster)
 
 # Extraindo os dados de Mapbiomas para cada polygono SIGEF
-
-
-unique(values(RJ_raster$brasil_coverage_2021)) %>% sort()
-
-
+# 1. identificar as classes de cobertura. 1 = floresta; 2 = não floresta
+# 2. filtrar (crop) e mascarar (mask) a área do poligono (propriedade) no raster mapbiomas
+# 3. projetar o raster do polígono (albers)
+# 4. Reclassificar os pixels
 
 # Matrix de reclassificação
-
-is <- c(3, 4, 5, 0, 9, 11, 15, 21, 23, 24, 25, 29, 30, 31, 32, 33, 39, 41, 49)
-becomes <- c(1, 1, 1, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0,  0, 0, 0, 0, 0,  0)
+unique(values(RJ_raster$brasil_coverage_2021)) %>% sort()
+is <- c(3, 4, 5, 49, 0, 9, 11, 15, 21, 23, 24, 25, 29, 30, 31, 32, 33, 39, 41)
+becomes <- c(1, 1, 1, 1, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0,  0, 0, 0, 0, 0)
 reclass_matrix <- matrix(c(is, becomes), ncol = 2)
 
-RJ_raster <- rast(RJ_raster)
-RJ_raster <- terra::classify(RJ_raster, rcl = reclass_matrix )
-RJ_raster$brasil_coverage_2021
-plot(RJ_raster$brasil_coverage_2021)
+# Projeção albers para calculo de área
+
+albers <- "+proj=aea +lat_1=-5 +lat_2=-42 +lat_0=-32 +lon_0=-60 +x_0=0 +y_0=0 +ellps=aust_SA +units=m +no_defs"
+
+# Criando lista para armazenar o resultado
 
 poly <- list()
 
 for(i in 1:nrow(sigef_3)){
   message(i)
-  poly[i] <- crop(RJ_raster, sigef_3[i,]) %>% 
-    mask(sigef_3[i,])
+  # i = 499
+  poly.i <-  terra::crop(RJ_raster, sigef_3[i,]) %>% 
+    terra::mask(sigef_3[i,])
+ 
+  
+  poly[[i]] <- poly.i %>% 
+    terra::project(albers, method = "near") %>% 
+    terra::classify(rcl = reclass_matrix )
 }
-
-plot(poly[[3]])
-
 
 # Calculando a área de florestas
 
@@ -153,27 +112,59 @@ for (i in 1:length(poly)) {
   list_results [[i]] <- as.data.frame(poly[[i]]) %>% 
     group_by(brasil_coverage_2021) %>% 
     summarise(pixel = n()) %>% 
-    mutate(area = pixel *30 *30)
+    mutate(area_m2 = pixel * res(poly[[i]])[1] * res(poly[[i]])[2])
 }
 
 list_results[[3]]
 
+# Inserindo o calculo de área na tabela sigef
+
+
 sigef_3 <- sigef_3 %>% 
-  mutate(area_for = NA)
+  mutate(area_for_m2 = NA)
 
 for(i in 1:nrow(sigef_3)){
   #i = 3
+  # i = 499
   if(nrow(list_results[[i]]) == 2){
     df.i <- list_results[[i]] %>% 
       as_tibble()
-  sigef_3$area_for[i] <- df.i[df.i$brasil_coverage_2021 == 1, "area"] %>% pull()} else {
-    sigef_3$area_for[i] = 0
+  sigef_3$area_for_m2[i] <- df.i[df.i$brasil_coverage_2021 == 1, "area_m2"] %>% pull()} else {
+    sigef_3$area_for_m2[i] = 0
   }
 }
 
 
+# Tabela Final
+colnames(sigef_3)
+sigef_3
+
+# Transformando os dados em dataframe
+
+st_geometry(sigef_3) <- NULL
+class(sigef_3)
 
 
-
-
+write_csv(sigef_3, "Resultados/tabela_final_sigef.csv")
 # rm(list = ls())
+
+library(tmap)
+RJ_map <- tm_shape(RJ_raster)+
+  tm_raster(title = "")+
+  tm_layout(main.title = "Classes Mapbiomas 2021")
+tmap_save(RJ_map, "Resultados/mapa_rj.png")  
+
+
+# para fazer os próximos mapas, rodei novamente o script até iniciar o for do raster. Então, rodei apenas 1 i (499), para gerar o poly.i. Alguma coisa no restante do script não permitir ter poly.i sem rodar tudo novamente
+
+
+poly_map <- tm_shape(poly.i)+
+  tm_raster(title = "")+
+  tm_layout(main.title = "Classes Propriedade")
+tmap_save(poly_map, "Resultados/propriedade.png")  
+
+poly_reclass <- tm_shape(poly.i)+
+  tm_raster(title = "")+
+  tm_layout(main.title = "Propriedade Classificada",
+            )
+tmap_save(poly_reclass, "Resultados/propriedade_classificada.png")  
